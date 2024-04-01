a02c639b45e4606a6a31b4598f7776c908083442
laurl <laurlumezeanu@gmail.com>
2024-04-01 20:22:33 +0300

    Tema5

---
+++ src/Tema5.java
@@ -0,0 +1,87 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public static class Tema5 {
+    public Tema5(String[] args) {
+        int x = 5;
+        int y = 10;
+        int suma = x + y;
+        System.out.println(suma);
+    }
+}
+public static void  numerePare(String[] args)  {
+    Scanner sc=new Scanner(System.in);
+     int n = sc.nextInt();
+    int[] arr = { 2,3,5,8,4 };
+    arr = new int[n];
+    System.out.println("Introducere elemente: ");
+    for(int i=0;i<n;i++)        {
+        arr[i]=sc.nextInt(); }
+    System.out.println("Numerele pare sunt: ");
+    for(int i=0;i<n;i++)     {
+        if(arr[i]%2==0)
+        {
+            System.out.print(STR."\{arr[i]} ");
+        }
+    }
+}
+public class Return {
+    public static void numereIntregi(String[] args)  {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nr = new int[]{2, 3, 4, 5, 6, 7};
+        n = 10;
+                for(int i=0;i<n;i++)        {
+           nr [i]=sc.nextInt();
+            System.out.println(i);
+        }
+    }
+    }
+
+public void main() {
+}
+public  class donatii {
+    public static void donatii(String[] args) {
+
+        Random r = new Random();
+        int randomDonatii = r.nextInt();
+        int i = 0;
+        int donate = 0;
+
+        donate = (int) (Math.random() * i);
+        if ( i = 0;
+        i < donate(i);
+        i++; )
+        System.out.println(i + (i + 1));
+
+        System.out.println("Succes!");
+    }
+}
+
+
+public  class donatiiMaxime {
+    private static Random r;
+
+    public static void maxim(int args) {
+        Random m = new Random();
+        int randomMaxim = r.nextInt();
+        int i = 0;
+        int maxim = 0;
+        maxim = (int) (Math.random() * i);
+        if ( i = 0;
+        i < randomMaxim(i);
+        i++; ) {
+
+            i == 1000;
+        }
+                System.out.println("Succes!");
+            }
+        }
+        public static void fraza(String[] args) {
+
+            String myString = "An apple a day. Keeps the doctor away! Good Morning. You wonders of Nature. Today is a great day to be happy.!";
+            System.out.println(myString);
+        }
+    }
+}
+
